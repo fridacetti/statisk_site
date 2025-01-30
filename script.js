0d3d4d1d2d1ff57f319cfac60e8659079144455c
fridacetti <frjo0003@stud.kea.dk>
2025-01-30 15:05:22 +0100

    dynamisk single done

---
+++ script.js
@@ -0,0 +1,33 @@
+let productContainer = document.querySelector(".productContainer");
+let productId = 1163;
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    productContainer.innerHTML = `
+      <div class="produktside">
+            <img class="produkt" src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="">
+            <div class="produkt_info">
+                <h1>Product Information</h1>
+                <div>
+                    <p class="fed"> Model name</p>
+                    <p> ${data.productdisplayname}</p>
+                    <p class="fed">Color</p>
+                    <p>${data.basecolour}</p>
+                    <p class="fed">Inventory number</p>
+                    <p>${data.id}</p>
+                </div>
+                <div class="info_2">
+                    <h1>${data.brandname}</h1>
+                    <p>Nike, creating experiencesfor today's athlete</p>
+                </div>
+            </div>
+            <div class="more-info">
+                <h1>${data.productdisplayname}</h1>
+                <p> 
+                ${data.brandname} | ${data.articletype} </p>
+                <p>Choose a size</p>
+                <p class="knap">Add to basket</p>
+            </div>
+        </div>
+        `;
+  });
